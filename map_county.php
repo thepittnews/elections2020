c7bf02c0c7bc7c98750ee7dd84541db3fb54ae62
Jon Moss <me@jonathanmoss.me>
2020-10-15 18:13:46 -0400

    County Map: Replace D3 with Leaflet

--- map_county.php
+++ map_county.php
@@ -1,37 +1,34 @@
 <!DOCTYPE html>
 <head>
-  <script src="https://d3js.org/d3.v5.min.js"></script><br />
-  <link href="https://fonts.googleapis.com/css?family=Bebas+Neue|Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <script src="http://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""/>
+  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+    crossorigin=""></script>
+
   <style>
+    /*#map {
+      height: 600px;
+      width: 1000px;
+    }*/
+
     body {
-      background-color: #c6e6f5;
+      padding: 0;
+      margin: 0;
     }
 
-    .map {
-      margin: auto;
-      text-align: center;
-    }
-
-    .border {
-      stroke-width: 1px;
-    }
-
-    @media screen and (max-width: 600px) {
-      .map {
-        width: 100%;
-      }
-    }
-
-    @media screen and (min-width: 601px) {
-      .map {
-        width: 70%;
-      }
+    html, body, #map {
+      height: 100%;
+      width: 100vw;
     }
   </style>
 </head>
 
 <body>
-  <div class="map"></div>
+  <div id="map"></div>
 
   <?php
     $csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTmwphY6oZEgjhGbyNKyFWI_VqDPBIyvLoYxIasPA7ZbwKup195iTyTm1aw8Gwcb1eLl0oOLkGexKXl/pub?gid=449653435&single=true&output=csv';
@@ -45,102 +42,82 @@
   ?>
 
   <script>
-    function responsivefy(svg) {
-      // Get container + svg aspect ratio
-      var width = parseInt(svg.style("width")),
-          height = parseInt(svg.style("height")),
-          aspect = width / height;
-
-      // Add viewBox and preserveAspectRatio properties,
-      // and call resize so that svg resizes on inital page load
-      svg.attr("viewBox", "0 0 " + width + " " + height)
-        .attr("perserveAspectRatio", "xMinYMid")
-        .call(resize);
-
-      d3.select(window).on("resize." + container.attr("class"), resize);
-
-      // Get width of container and resize svg to fit it
-      function resize() {
-        var targetWidth = parseInt(container.style("width"));
-        svg.attr("width", targetWidth);
-        svg.attr("height", Math.round(targetWidth / aspect));
-      };
-    };
-
-    d3.selection.prototype.moveToFront = function() {
-      return this.each(function() {
-        this.parentNode.appendChild(this);
-      });
-    };
-
-    const w = 1000;
-    const h = 600;
-    const container = d3.select("div.map");
-
-    const svg = container.append("svg")
-      .attr("width", w)
-      .attr("height", h);
-    responsivefy(svg);
-
     const countyData = <?php echo json_encode($rows) ?>;
+    const map = L.map('map', {
+      doubleClickZoom: false,
+      scrollWheelZoom: false,
+      touchZoom: false,
+      zoomControl: false,
+      zoomSnap: 0.1
+    });
 
-    const democratColor = d3.scaleLog().domain([1, d3.max([100])])
-      .interpolate(d3.interpolateRgb)
-      .range([d3.rgb('#ceeafd'), d3.rgb('#006aab')]);
+    const tilelayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+      subdomains: 'abcd',
+      maxZoom: 19
+    }).addTo(map);
+    map.setView([40.4350, -79.9959], 10.3);
 
-    const republicanColor = d3.scaleLog().domain([1, d3.max([100])])
-      .interpolate(d3.interpolateRgb)
-      .range([d3.rgb('#fbd0d0'), d3.rgb('#b02029')]);
-
-    // Boot up graphic
-    Promise.all([ d3.json("/geo_county.geojson"), d3.json("/geo_rivers.geojson") ])
-    .then(function([ countyGeoData, riversGeoData ]) {
-      for(var countyFeature of countyGeoData.features) {
-        const municipality = countyFeature.properties.NAME;
+    $.getJSON('/geo_county.geojson', (geoJSONData) => {
+      for(var feature of geoJSONData.features) {
+        const municipality = feature.properties.NAME;
         var countyDataRow = countyData.find((c) => c[0] === municipality);
         if (!countyDataRow) {
-          countyDataRow = countyData.find((c) => c[0] === `${countyFeature.properties.NAME} ${countyFeature.properties.TYPE}`);
+          countyDataRow = countyData.find((c) => c[0] === `${feature.properties.NAME} ${feature.properties.TYPE}`);
         }
 
-        countyFeature.properties["pctTotal"] = countyDataRow[1];
-        countyFeature.properties["pctReporting"] = countyDataRow[2];
-        countyFeature.properties["bidenPct"] = countyDataRow[3];
-        countyFeature.properties["bidenVotes"] = countyDataRow[4];
-        countyFeature.properties["trumpPct"] = countyDataRow[5];
-        countyFeature.properties["trumpVotes"] = countyDataRow[6];
+        feature.properties["pctTotal"] = countyDataRow[1];
+        feature.properties["pctReporting"] = countyDataRow[2];
+        feature.properties["bidenPct"] = countyDataRow[3];
+        feature.properties["bidenVotes"] = countyDataRow[4];
+        feature.properties["trumpPct"] = countyDataRow[5];
+        feature.properties["trumpVotes"] = countyDataRow[6];
       }
 
-      const projection = d3.geoMercator();
-      const path = d3.geoPath().projection(projection);
-      projection.fitSize([w, h], countyGeoData); // Adjust the projection to the features
+      const getFillColor = (feature) => {
+        if (Number(feature.properties.bidenVotes.replace(',', '')) > Number(feature.properties.trumpVotes.replace(',', ''))) {
+          return Number(feature.properties.bidenPct) > 70 ? '#006aab' :
+                 Number(feature.properties.bidenPct) > 60 ? '#6193c7' :
+                 Number(feature.properties.bidenPct) > 50 ? '#9cc0e3' : '#ceeafd';
+        } else {
+          return Number(feature.properties.trumpPct) > 70 ? '#b02029' :
+                 Number(feature.properties.trumpPct) > 60 ? '#cf635d' :
+                 Number(feature.properties.trumpPct) > 50 ? '#e99d98' : '#fbd0d0';
+        }
+      };
 
-      const g = svg.append("g"); countyGeoData
-      g.selectAll("path").data(countyGeoData.features.concat(riversGeoData.features))
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .attr("class", "border")
-        .attr("stroke", "#000000")
-        .attr("stroke-width", "1px")
-        .attr("fill", function(d) {
-          if (d.properties.NAME.endsWith(' River') || d.properties.NAME === " ") {
-            return '';
-          } else if (Number(d.properties["bidenVotes"].replace(',', '')) > Number(d.properties["trumpVotes"].replace(',', ''))) {
-            return democratColor(d.properties["bidenPct"]);
-          } else {
-            return republicanColor(d.properties["trumpPct"]);
-          }
-        })
-        .on("mouseover", function(d) {
-          d3.select(this).attr("stroke", "#ffb81c").attr("stroke-width", "2px").moveToFront();
-          d3.select("#county_hover").text(d.properties["county_nam"]);
-          d3.select("#cases_hover").text(d3.format(",d")(d.properties["trumpPct"]));
-        })
-        .on("mouseout", function() {
-          d3.select(this).attr("stroke", "#000000")
-          d3.select("#county_hover").text("");
-          d3.select("#cases_hover").text("");
-        });
+      const style = (feature) => {
+        return {
+          color: '#000000',
+          fillColor: getFillColor(feature),
+          fillOpacity: 0.8
+        };
+      }
+
+      const geoJSON = L.geoJSON(geoJSONData, {
+        onEachFeature: (feature, layer) => {
+          layer.on('mouseover', function () {
+            const tooltipText = `<div>
+              <h3>${feature.properties.NAME}</h3>
+              <p>Biden Votes: ${feature.properties.bidenVotes}, Pct: ${feature.properties.bidenPct}</p>
+              <p>Trump Votes: ${feature.properties.trumpVotes}, Pct: ${feature.properties.trumpPct}</p>
+              <p>${feature.properties.pctReporting} of ${feature.properties.pctTotal} in-person precincts reporting</p>
+            </div>`;
+            layer.bindTooltip(tooltipText).openTooltip();
+
+            this.setStyle({ color: '#ffb81c' });
+
+            if (!L.Browser.ie && !L.Browser.opera) {
+              this.bringToFront();
+            }
+          });
+
+          layer.on('mouseout', function () {
+            this.setStyle({ color: '#000000' });
+          });
+        },
+        style
+      }).addTo(map);
     });
   </script>
 </body>
