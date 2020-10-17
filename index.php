64b3128cfbb93473d4ede55bf78355ce55721ad4
Jon Moss <me@jonathanmoss.me>
2020-10-16 23:05:46 -0400

    Combine to one map JS file

--- index.php
+++ index.php
@@ -143,8 +143,7 @@
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
       integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
       crossorigin=""></script>
-    <script src="/map_county.js"></script>
-    <script src="/map_pa.js"></script>
+    <script src="/maps.js"></script>
   </head>
 
   <body>
